b2e2e376836ec348a4294e760039d3e4381d4a98
WebScript <Paul1410@yandex.ru>
2019-12-11 21:12:29 +0300

    add is three files

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-var IsShop = React.createClass({
-
+let IsShop = React.createClass({
+  
     displayName: 'IsShop',
   
     render: function(){
-  
-      return React.DOM.div( {className:'IsShopFrame'}, 
-        React.DOM.h1( null, "Всем привет!" ),
-        React.DOM.div( {className:'IsShopText'}, "Начинаем изучение React!" ),
-      );
-    },
-  
-  });
+    let ok = [];
+    let fun = [];
+                  this.props.item.forEach(function(item, index){
+                      keyItem = index, nameItem = item.name, modelItem = item.model, costItem = item.cost
+                     fun =
+                        React.DOM.div({key:keyItem, className: "Item"},
+                          React.DOM.p( null, nameItem ),
+                          React.DOM.div( {className: "photo"},
+                              React.DOM.p( null, "this is photo of item")
+                              ),
+                            React.DOM.p(null, modelItem),
+                           // React.DOM.div({className:"divP"},     
+                             //  React.Dom.p(null, costItem),
+                         //)    
+                        )
+                        ok.push(fun)
+                  })
+    return React.DOM.div( {className:'IsShopFrame'},
+              React.DOM.div({className:'header'},
+              React.DOM.h1(null, "STORE REACT")), 
+              React.DOM.div({className: "ItemDiv"},ok),
+            )
+       },
+     });
+
+ 
